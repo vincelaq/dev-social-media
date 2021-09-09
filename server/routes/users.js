dcbeb51335460297cb71c2d774b5f29634f63fde
Vincent Laquindanum <laq.vince@gmail.com>
2021-09-08 21:24:57 -0700

    add image upload feature for banner and avatar

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,12 +2,13 @@
 const router = require("express").Router();
 const { users } = require("../controllers");
 const auth = require("../middleware/auth");
-const fileUpload = require("../middleware/upload");
+const upload = require("../middleware/upload");
+
 
 router.get("/", users.index);
 router.get("/profile", auth, users.getMyProfile);
 router.get("/profile/:uid", users.getUserProfile);
-router.put("/profile", [ auth, fileUpload.single('image') ], users.updateMyProfile);
+router.put("/profile", auth, upload.fields([{ name: 'image', maxCount: 1 }, { name: 'banner', maxCount: 1 }]), users.updateMyProfile);
 router.delete("/", auth, users.destroyUser);
 
 module.exports = router;
