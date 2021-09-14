d8fae2614484ef3bf28f2de7162917ac75054643
James Bell <jamesbell@Jamess-MacBook-Pro.local>
2021-09-13 17:58:46 -0700

    working on profile page

--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -1,10 +1,38 @@
-import React from "react";
-
+import React, {useState, useEffect } from "react";
+import axios from "axios";
+import Post from "../../components/Post";
 import './style.css';
 
 const ProfilePage = () => {
+    const [posts, setPosts] = useState([]);
+
+    const fetchPosts = async () => {
+        let res = await axios ({
+            method: "get",
+            url: "http://localhost:500-/api/posts/user/:uid",
+            // replace uid with a different parameter
+        })
+
+        if (res.status === 200) {
+            setPosts(res.data.data.reverse());
+        }
+        console.log("Post Incoming", res);
+    }
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
     return (
-        <p>Profile Page</p>
+        <div>
+            ProfilePage
+            {posts.map((post) => {
+                return (
+                    <Post
+                )
+            })}
+        </div>
+       
     )
 }
 
