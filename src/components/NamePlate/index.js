79d1b981507401b31f456369370365394b853e3a
James Bell <jamesbell@Jamess-MacBook-Pro.local>
2021-09-14 21:24:51 -0700

    add Follow button

--- src/components/NamePlate/index.js
+++ src/components/NamePlate/index.js
@@ -1,17 +1,30 @@
 import React from "react";
 import "./style.css";
 
-const NamePlate = ({username, jobTitle, image, bio, numberOfPosts, numberOfConnections}) => {
-    return(
-       <div className="NamePlate-container">
-            <div className="image"> {image} </div>
-            <div className="username"> {username} </div>
-            <div className="jobTitle"> {jobTitle} </div>
-            <div className="bio"> {bio} </div>
-            <div className="numberOfPosts"> Posts: {numberOfPosts} </div>
-            <div className="numberOfConnections"> Connections: {numberOfConnections} </div>
-       </div> 
-    )
+const NamePlate = ({
+  username,
+  jobTitle,
+  image,
+  bio,
+  numberOfPosts,
+  numberOfConnections,
+}) => {
+  return (
+    <div className="NamePlate-container">
+      <button class="follow-button">Follow</button>
+      <div className="image"> {image} </div>
+      <div className="username"> {username} </div>
+      <div className="jobTitle"> {jobTitle} </div>
+      <div className="postAndConnections-container">
+        <div className="numberOfPosts"> {numberOfPosts} Posts: </div>
+        <div className="numberOfConnections">
+          {" "}
+          {numberOfConnections} Connections:{" "}
+        </div>
+      </div>
+      <div className="bio"> {bio} </div>
+    </div>
+  );
 };
 
 export default NamePlate;
