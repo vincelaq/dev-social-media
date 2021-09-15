ca10921e77bd20f45b56d6d49c93ff691dbf5b82
James Bell <jamesbell@Jamess-MacBook-Pro.local>
2021-09-14 19:52:39 -0700

    name plate css border and padding in place

--- src/components/NamePlate/index.js
+++ src/components/NamePlate/index.js
@@ -3,13 +3,13 @@ import "./style.css";
 
 const NamePlate = ({username, jobTitle, image, bio, numberOfPosts, numberOfConnections}) => {
     return(
-       <div>
+       <div className="NamePlate-container">
+            <div className="image"> {image} </div>
             <div className="username"> {username} </div>
             <div className="jobTitle"> {jobTitle} </div>
-            <div className="image"> {image} </div>
             <div className="bio"> {bio} </div>
-            <div className="numberOfPosts"> {numberOfPosts} </div>
-            <div className="numberOfConnections"> {numberOfConnections} </div>
+            <div className="numberOfPosts"> Posts: {numberOfPosts} </div>
+            <div className="numberOfConnections"> Connections: {numberOfConnections} </div>
        </div> 
     )
 };
