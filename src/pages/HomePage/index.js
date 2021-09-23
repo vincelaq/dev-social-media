dea4ff5b26183bcd1586998bd28b6b3f7f7b7a2f
Vincent Laquindanum <laq.vince@gmail.com>
2021-09-22 22:48:25 -0700

    add sorting visuals for homepage

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -2,18 +2,50 @@ import React, { useContext } from "react";
 import Post from "../../components/Post";
 import { AuthContext } from '../../context/auth-context';
 
-import "../../index.css"
+import "./style.css"
 
 const HomePage = ({posts, fetchPosts}) => {
     const auth = useContext(AuthContext);
     const user = auth.user;
     
+    const handleAllSort = () => {
+        const allSort = document.getElementById('all-sort');
+        const followingSort = document.getElementById('following-sort');
+
+        if (!allSort.classList.contains("active")) {
+            allSort.classList.add('active');
+            followingSort.classList.remove('active')
+            // insert function for all sort
+        }
+    }
+
+    const handleFollowingSort = () => {
+        const allSort = document.getElementById('all-sort');
+        const followingSort = document.getElementById('following-sort');
+
+        if (!followingSort.classList.contains("active")) {
+            allSort.classList.remove('active');
+            followingSort.classList.add('active')
+            // insert function for following sort
+        }
+    }
+
     return (
         <div>
             <section>
                 <div className="container">
-                    <h1><span className="--text-green">Hello, {user.username}</span><br/>
+                    <h1 className="welcome-header"><span className="--text-green">Hello, {user.username}</span><br/>
                     Welcome Back!</h1>
+                    <div className="dashboard__wrapper">
+                        
+                        <div className="dashboard__title">Dashboard</div>
+                        <div id="all-sort" className="dashboard__button active" onClick={handleAllSort} >
+                            <span className="dashboard__sort">All Posts</span>
+                        </div>
+                        <div id="following-sort" className="dashboard__button" onClick={handleFollowingSort}>
+                            <span className="dashboard__sort">Following</span>
+                        </div>   
+                    </div>
                     {posts.map((post) => {
                         return (
                             <Post
